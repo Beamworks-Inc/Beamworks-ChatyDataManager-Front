807e8a3fbb5b98d4b840de201285b513d4c8a7c9
BEOKS <lee01042000@gmail.com>
2023-01-06 16:35:15 +0900

    Feat : FolderTree API 코드 작성
    - axios 패키지 추가
    - FolderAPI 인터페이스 작성
    - FolderAPI 구현체 작성

--- src/apis/folder.ts
+++ src/apis/folder.ts
@@ -1,50 +1,36 @@
 import api from "./index";
 import { Folder } from "interfaces/Content.interface";
+import {AxiosResponse} from "axios";
 
-// Folders
-export const createFolderTree = async (folder: Folder) => {
-  const url = "/folder";
-  try {
-    const response = await api.post<Folder>(url, { folder });
-    return response.data;
-  } catch (error: any) {
-    const { status, statusText } = error.response;
-    alert(`Error: (Fail to createFolderTree), ${status} ${statusText}`);
-  }
-};
+interface IFoldersAPI {
+    findAllRoot: ()=>Promise<AxiosResponse<Folder[]>>,
+    find: (folderId: string)=>Promise<AxiosResponse<Folder>>,
+    update: (folder: Folder)=>Promise<AxiosResponse<null>>,
+    create: (folder: Folder)=>Promise<AxiosResponse<null>>,
+    delete: (folderId: string)=>Promise<AxiosResponse<null>>,
+    deleteAll : ()=>Promise<AxiosResponse<null>>,
+}
+const URI = "/folder";
+/**
+ * axios 를 이용하여 서버에 요청을 보내는 함수들을 정의합니다.
+ * @example
+ * ```typescript
+ * FoldersAPI.findAllRoot()
+ *  .then((res)=>{console.log(res.data)})
+ *  .error((err)=>{console.log(err)})
+ * ```
+ */
+const FoldersAPI: IFoldersAPI = {
+  findAllRoot: ()=> api.get(`${URI}`),
+  find: (folderId: string) => api.get(`${URI}/${folderId}`),
+  create: (folder: Folder) => api.post(`${URI}`, folder),
+  update: (folder: Folder) => api.put(`${URI}/1`, folder),
+  delete: (folderId: string) => api.delete(`${URI}/${folderId}`),
+  deleteAll: ()=>api.delete(`${URI}`),
+}
 
-export const getFolderTree = async () => {
-  const url = "/folder";
-  try {
-    const response = await api.get<Folder>(url);
-    return response.data; // root folder
-  } catch (error: any) {
-    const { status, statusText } = error.response;
-    alert(`Error: (Fail to getFolderTree), ${status} ${statusText}`);
-  }
-};
-
-export const updateFolderTree = async (
-  originName: String,
-  destName: Folder
-) => {
-  const url = "/folder";
-  try {
-    const response = await api.put<Folder>(url, { originName, destName });
-    return response.data;
-  } catch (error: any) {
-    const { status, statusText } = error.response;
-    alert(`Error: (Fail to updateFolderTree), ${status} ${statusText}`);
-  }
-};
-
-export const deleteFolderTreeItem = async (name: string) => {
-  const url = `/folder/${name}`;
-  try {
-    const response = await api.delete<Folder>(url);
-    return response.data;
-  } catch (error: any) {
-    const { status, statusText } = error.response;
-    alert(`Error: (deleteFolderTreeItem), ${status} ${statusText}`);
-  }
-};
+export default FoldersAPI;
+const a:Folder = {
+    name: "init",
+    children: [],
+}
