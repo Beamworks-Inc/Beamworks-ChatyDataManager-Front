a49d27af739ec3ecb9b30cad0dad36a4b7922510
BEOKS <lee01042000@gmail.com>
2023-01-11 13:36:20 +0900

    Fix : 역할을 클릭하면 사용자 역할 API 를 호출하도록 수정

--- src/pages/authentication/AuthenticateState/RoleSelectDialog.tsx
+++ src/pages/authentication/AuthenticateState/RoleSelectDialog.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import {Button, Dialog, DialogTitle} from "@mui/material";
 import {useDispatch} from "react-redux";
 import {UserAction} from "./UserReducer";
+import AuthAPI, {Role} from "../../../apis/Auth";
 
 const RoleSelectDialog=()=>{
     const dispatch=useDispatch()
     const navigateToMain=()=>{
         window.location.href="/"
     }
-    const handleCreatorButton=()=>{
-        dispatch(UserAction.setUserRole("USER"))
-        navigateToMain()
-    }
-    const handleReviewerButton=()=>{
-        dispatch(UserAction.setUserRole("REVIEWER"))
-        navigateToMain()
+    const setRole=(role: Role)=>()=>{
+        AuthAPI.updateUserRole(role)
+            .then(response=>{
+                dispatch(UserAction.setUserRole(role))
+                navigateToMain()
+            })
+            .catch(error=>{
+                alert('사용자 정보 업로드 실패')
+            })
     }
     return(
         <Dialog open={true}>
             <DialogTitle>Choose Role</DialogTitle>
-            <Button onClick={handleCreatorButton}> Creator</Button>
-            <Button onClick={handleReviewerButton}> Reviewer</Button>
+            <Button onClick={setRole("USER")}> Creator</Button>
+            <Button onClick={setRole("REVIEWER")}> Reviewer</Button>
         </Dialog>
     )
 }
