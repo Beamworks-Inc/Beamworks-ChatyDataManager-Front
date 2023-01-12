984e6877289fe2cb57ed8cae8cf8248adaa3aac8
Henry-Hong <hhr8540@naver.com>
2023-01-12 09:06:50 +0900

    Fix : Dev 브랜치로 머지중, Test 코드의 인자가 없어서 생긴 빌드에러 해결 2

--- test/content.test.tsx
+++ test/content.test.tsx
@@ -7,24 +7,23 @@ describe("생성자가 처음 컨텐츠를 생성하면 컨텐츠의 state는 DF
 			// When
 			// Then
 		});
-		it("");
 	});
 });
 
-it(
-	"컨텐츠의 state가 DRAFT 상태인 경우 컨텐츠는 작성자만 수정 및 확인 할 수 있다."
-);
-it(
-	"컨텐츠의 state가 REVIEW 상태인 경우 컨텐츠는 검수자만 수정 및 확인 할 수 있다."
-);
-it(
-	"컨텐츠의 state가 REVIEW 상태인 경우, 검수자는 컨텐츠의 state를 REJECTED로 변경할 수 있다."
-);
-it(
-	"컨텐츠의 state가 REJECT로 상태인 경우 컨텐츠는 작성자만 수정 및 확인 할 수 있다."
-);
+// it(
+// 	"컨텐츠의 state가 DRAFT 상태인 경우 컨텐츠는 작성자만 수정 및 확인 할 수 있다."
+// );
+// it(
+// 	"컨텐츠의 state가 REVIEW 상태인 경우 컨텐츠는 검수자만 수정 및 확인 할 수 있다."
+// );
+// it(
+// 	"컨텐츠의 state가 REVIEW 상태인 경우, 검수자는 컨텐츠의 state를 REJECTED로 변경할 수 있다."
+// );
+// it(
+// 	"컨텐츠의 state가 REJECT로 상태인 경우 컨텐츠는 작성자만 수정 및 확인 할 수 있다."
+// );
 
-it(
-	"컨텐츠의 state가 REJECTED 또는 DRAFT 상태인 경우에 작성자는 APPROVE 상태로 변경할 수 있다."
-);
-it("컨텐츠가 REVIEW 상태인 경우 검수자가 APPROVE 상태로 변경할 수 있다.");
+// it(
+// 	"컨텐츠의 state가 REJECTED 또는 DRAFT 상태인 경우에 작성자는 APPROVE 상태로 변경할 수 있다."
+// );
+// it("컨텐츠가 REVIEW 상태인 경우 검수자가 APPROVE 상태로 변경할 수 있다.");
