33fb56182240675d278db98e0ba327b91bc8b496
Henry-Hong <hhr8540@naver.com>
2023-01-18 10:07:03 +0900

    Fix : ContentList 페이지에서 Datagrid에 있는 컨텐츠 클릭시 navigate 수정
    - CustomDatagrid 여러 파일로 분리, 주석 추가

---
+++ src/components/CustomDatagrid/columns.tsx
@@ -0,0 +1,91 @@
+export const columns = [
+	// 1. 특정 칼럼의 값을 다른 칼럼을 통해 계산할 수 있습니다.
+	// valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`
+	// 2. 마우스 오버시 툴팁을 표시할 수 있습니다.
+	// description: 'This column has a value getter and is not sortable.',
+	{ field: "id", headerName: "ID", width: 90, sortable: false },
+	{
+		field: "question",
+		headerName: "Question",
+		width: 200,
+		sortable: false,
+		editable: false,
+	},
+	{
+		field: "answer",
+		headerName: "Answer",
+		width: 200,
+		sortable: false,
+		editable: false,
+	},
+	{
+		field: "reference",
+		headerName: "Reference",
+		width: 110,
+		editable: false,
+		description: "논문 서치 정보, 웹페이지 링크등을 사용합니다.",
+		// @ts-ignore
+		renderCell: (params) => (
+			<a href={params.row.referenceLink}>{params.row.reference}</a>
+		),
+	},
+	{
+		field: "referenceLink",
+		headerName: "reference link",
+		width: 200,
+		hide: true,
+	},
+	{
+		field: "rationale",
+		headerName: "Rationale",
+		width: 110,
+		editable: false,
+		description: "A에 대한 부가 자료 (Link, Image, Movie)",
+		// @ts-ignore
+		renderCell: (params) => (
+			<a href={params.row.rationaleLink}>{params.row.rationale}</a>
+		),
+	},
+	{
+		field: "rationaleLink",
+		headerName: "rationale link",
+		width: 200,
+		hide: true,
+	},
+	{
+		field: "writer",
+		headerName: "Writer",
+		width: 110,
+		editable: false,
+	},
+	{
+		field: "writeDate",
+		headerName: "write date",
+		type: "date",
+		width: 110,
+		editable: false,
+		renderCell: (params: any) => {
+			return <div>{params.row.writeDate.slice(0, 10)}</div>;
+		},
+	},
+	{
+		field: "reviewer",
+		headerName: "Reviewer",
+		width: 110,
+		editable: false,
+	},
+	{
+		field: "reviewDate",
+		headerName: "review date",
+		type: "date",
+		width: 110,
+		editable: false,
+	},
+	{
+		field: "status",
+		headerName: "status",
+		type: "string",
+		width: 110,
+		editable: false,
+	},
+];
