1c62d1a4d0dbcf15ebe3a02c27929fba02ed6c2d
BEOKS <lee01042000@gmail.com>
2023-01-25 09:52:25 +0900

    Fix : 사용자 인증 활성화

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import {useDispatch} from "react-redux";
 import {UserAction} from "./pages/authentication/AuthenticateState/UserReducer";
 
 function App() {
-    const dispatch = useDispatch();
-    dispatch(UserAction.setUser({
-        id: 1,
-        name: "김민수",
-        role: "USER",
-        email: ""
-    }));
+    // const dispatch = useDispatch();
+    // dispatch(UserAction.setUser({
+    //     id: 1,
+    //     name: "김민수",
+    //     role: "USER",
+    //     email: ""
+    // }));
   return (
     <ThemeCustomization>
       <ScrollTop>
