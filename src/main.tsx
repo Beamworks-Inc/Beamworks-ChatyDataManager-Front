6a2ded9c6fa03042c36fac6c90841ce472e977a8
BEOKS <lee01042000@gmail.com>
2023-02-02 11:25:01 +0900

    Fix(QAC-3) :  잘못된 Apply 진행 시 Upload 진행 창이 종료되지 않는 현상

--- src/main.tsx
+++ src/main.tsx
@@ -14,11 +14,9 @@ const store = createStore(rootReducer);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ReduxProvider store={store}>
-			<AuthenticatePage>
-				<BrowserRouter basename="/">
-					<App />
-				</BrowserRouter>
-			</AuthenticatePage>
+			<BrowserRouter basename="/">
+				<App />
+			</BrowserRouter>
 		</ReduxProvider>
 	</React.StrictMode>
 );
